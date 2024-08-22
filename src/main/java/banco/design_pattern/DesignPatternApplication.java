afd822a89f28472c63accce7f61d29b44a68c833
Gabriel Ribeiro da Silva Santos <84988124+GabrielRSSantos@users.noreply.github.com>
2024-08-22 13:42:50 -0300

    Cors bugfix

--- src/main/java/banco/design_pattern/DesignPatternApplication.java
+++ src/main/java/banco/design_pattern/DesignPatternApplication.java
@@ -11,9 +11,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 import java.util.List;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @SpringBootApplication
 public class DesignPatternApplication {
 
